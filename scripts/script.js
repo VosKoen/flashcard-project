f1b49b70e32c2ed3142559d6e4ecf4840f2f4bad
Prophetaa <klaux_15@hotmail.com>
2018-09-14 13:26:05 +0200

    added scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
-console.log("hey")
+const defaultSet ={
+  question:["How much is 2+2",
+  "what's the capital of Spain?"],
+  anwser:["4","Madrid"]
+}
+var counter = 0
+var objCounter = defaultSet.question.length
+var qCounter = Math.floor(Math.random() * objCounter)
+function divClick(){
+  if(counter === 0){
+    getQuestion()
+    return counter++
+} else if (counter === 1){
+  getAnswer();
+  return counter=0;
+}
+}
+var cardHeader = document.getElementById('flaschard-header')
+var cardContent = document.getElementById('flashcard-content')
+
+function getQuestion(){
+cardHeader.innerHTML ="Question"
+cardContent.innerHTML = defaultSet.question[qCounter]
+
+}
+
+function getAnswer(){
+  cardHeader.innerHTML ="Answer "
+  cardContent.innerHTML =`The Answer i: ${defaultSet.anwser[qCounter]}.`;
+}
